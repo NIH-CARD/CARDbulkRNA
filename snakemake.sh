8675885b60c250973529009dbc81ea9e2dc405c9
adamcatching <adamcatching@gmail.com>
2025-11-05 11:54:38 -0500

    Populate snakefile.sh

--- snakemake.sh
+++ snakemake.sh
@@ -1,1 +1,20 @@
-# Test line
+#!/bin/bash
+
+#SBATCH --cpus-per-task 1
+#SBATCH --mem-per-cpu=32G
+#SBATCH --time 96:00:00
+
+module purge
+module load apptainer
+
+# Pull profile, this will only run once, and is required for running on Biowulf
+git clone https://github.com/NIH-HPC/snakemake_profile.git
+
+# Load singularity
+module load singularity
+
+# Bind external directories on Biowulf
+. /usr/local/current/singularity/app_conf/sing_binds
+
+# RUN SCRIPT
+snakemake --cores all --profile snakemake_profile --use-singularity
